655c9e356a4d1dbea634a489e011d211b0f995cb
SamuelKauffman <112411760+SamuelKauffman@users.noreply.github.com>
2022-08-30 12:49:36 -0400

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -4,7 +4,8 @@ public static void main(String[] args)
 	{
 	int number1 = 3;
 	int number2 = 5;
-	int sum = number1 - number2;
-	System.out.println(sum);
+	System.out.println(number1 + number2);
+	System.out.println(number1 - number2);
+	System.out.println(number1 * number2);
 	}
 }
